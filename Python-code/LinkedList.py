16b17985c0846e5e59a8c176e9c6c47ad686d3c6
Afshan Alam <70933140+Afshan01@users.noreply.github.com>
2024-02-08 02:47:59 +0000

    Added linked list in python

---
+++ Python-code/LinkedList.py
@@ -0,0 +1,35 @@
+# type declaration for a linked list of integers: 
+
+# Node of a Singly Linked List
+class Node:
+    # constructor
+    def __init__(self, data = None, next = None):
+        self.data = data
+        self.next = next
+    
+    # method for setting the data fields of the node
+    def setData(self, data):
+        self.data = data
+
+    # method for getting the data fields of the node
+    def getData(self):
+        return self.data
+
+    # method for setting the next field of the node
+    def setNext(self, next):
+        self.next = next
+
+    # method for getting the next field of the node
+    def getNext(self):
+        return self.next
+    
+    # returns true if the node points to another node
+    def hasNext(self):
+        return self.next != None
+
+# class for defining a linked list
+class LinkedList(object):
+    # initializing a list
+    def __init__(self, node = None):
+        self.length = 0
+        self.head = node
